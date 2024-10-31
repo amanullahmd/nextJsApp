fe978716ae51b462ebefa60ed9f3bc2897932029
Md Amanullah <amanullah1998bd@gmail.com>
2024-10-31 16:54:12 +0100

    update

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import AddToCard from './AddToCard';
+import Styles from './ProductCard.module.css';
+
+
+const ProductCard = () => {
+  return (
+    <div >
+        
+        <AddToCard />
+    </div>
+  )
+}
+
+export default ProductCard
